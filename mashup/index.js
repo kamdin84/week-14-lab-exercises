cacdde9a912ee0bb15011d09bfd8691431416ed4
kamdin84 <u2238149@live.warwick.ac.uk>
2023-02-06 11:42:09 +0000

    fixed bracket situation

--- mashup/index.js
+++ mashup/index.js
@@ -1,8 +1,8 @@
-const a = 'b';
-console.log('Hello this is mashup js')
+const a = "b";
+console.log("Hello this is mashup js")
 
 if (myStatement = false)
 
 {
-  console.log('This is mashup' + 'strings'); if (true == false) { }
+  console.log("This is mashup" + "strings"); if (true == false) { }
 }
